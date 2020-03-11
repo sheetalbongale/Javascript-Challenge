740d2cd6167ba694f966295f3f23275650a5dab2
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-03-11 01:40:08 -0500

    Add the completed work

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
-# JavaScript and DOM Manipulation 
+# JavaScript and DOM Manipulation - UFO Sightings
 
-## Background
-Create a table dynamically based upon a [dataset we provide](StarterCode/static/js/data.js). We also need to allow our users to filter the table data for specific values. Languages for this assignment are: JavaScript, HTML, and CSS, and D3.js.
+![ufo sightings](static/images/ufo4.gif)
 
-### Level 1: Automatic Table and Date Search (Required)
+## Objective: 
+This challenge demonstrates creating an interactive webpage to display a table dynamically for the [UFO Sightings Data](static/js/data.js)allowing user to input their choice from the drop-down to filter the table data. 
 
-* Create a basic HTML web page or use the [index.html](StarterCode/index.html) file provided (we recommend building your own custom page!).
-
-* Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-
-  * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
-
-* Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
-
-### Level 2: Multiple Search Categories (Optional)
-
-* Complete all of Level 1 criteria.
-
-* Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
+The JavaScript code lets the user to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
 
   1. `date/time`
   2. `city`
@@ -25,8 +13,8 @@ Create a table dynamically based upon a [dataset we provide](StarterCode/static/
   4. `country`
   5. `shape`
 
-- - -
+## Languages used: 
+`JavaScript|  D3.js | HTML | CSS | Bootstrap`
 
-### Dataset
+Find the rendered web page here ➡️ [UFO Sightings](https://sheetalbongale.github.io/Javascript-Challenge/)
 
-* [UFO Sightings Data](StarterCode/static/js/data.js)
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,103 @@
 // from data.js
 const tableData = data;
 
-// YOUR CODE HERE!
+// declaring the table and filter buttons
+const table = d3.select("#ufo-table");
+const tbody = table.select("tbody");
+const filterButton = d3.select("#filter-btn");
+const resetButton = d3.select("#clear-filter-btn");
+
+// find all the unique values in the data to display for the user to select
+const dateList = tableData.map(item => item.datetime)
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
+const cityList = tableData.map(item => item.city)
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
+const stateList = tableData.map(item => item.state)
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
+const countryList = tableData.map(item => item.country)
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
+const shapeList = tableData.map(item => item.shape)
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
+
+// dropdown list
+dateList.forEach(item => {
+    d3.select("#datetime_list").append("option").append("value").text(item)
+});
+cityList.forEach(item => {
+    d3.select("#city_list").append("option").append("value").text(item)
+});
+stateList.forEach(item => {
+    d3.select("#state_list").append("option").append("value").text(item)
+});
+countryList.forEach(item => {
+    d3.select("#country_list").append("option").append("value").text(item)
+});
+shapeList.forEach(item => {
+    d3.select("#shape_list").append("option").append("value").text(item)
+});
+
+// declare the constants for the filter input buttons
+const inputDate = d3.select("#datetime");
+const inputCity = d3.select("#city");
+const inputState = d3.select("#state");
+const inputCountry = d3.select("#country");
+const inputShape = d3.select("#shape");
+
+// assign function to handler
+const handler = function() {
+
+    tbody.html("");
+
+    let filterDate = inputDate.property("value").trim();
+    let filterCity = inputCity.property("value").toLowerCase().trim();
+    let filterState = inputState.property("value").toLowerCase().trim();
+    let filterCountry = inputCountry.property("value").toLowerCase().trim();
+    let filterShape = inputShape.property("value").toLowerCase().trim();
+
+    let filteredData = [];
+
+    if (filterDate !== "") {
+        filteredData = tableData.filter(dataRow => dataRow.datetime === filterDate)
+    } else filteredData = tableData;
+    if (filterCity !== "") {
+        filteredData = filteredData.filter(dataRow => dataRow.city === filterCity)
+    };
+    if (filterState !== "") {
+        filteredData = filteredData.filter(dataRow => dataRow.state === filterState)
+    };
+    if (filterCountry !== "") {
+        filteredData = filteredData.filter(dataRow => dataRow.country === filterCountry)
+    };
+    if (filterShape !== "") {
+        filteredData = filteredData.filter(dataRow => dataRow.shape === filterShape)
+    };
+    
+    console.log(filteredData);
+
+    filteredData.forEach(dataRow => {
+    let row = tbody.append("tr");
+    row.append("td").text(dataRow.datetime);
+    row.append("td").text(dataRow.city);
+    row.append("td").text(dataRow.state);
+    row.append("td").text(dataRow.country);
+    row.append("td").text(dataRow.shape);
+    row.append("td").text(dataRow.durationMinutes);
+    row.append("td").text(dataRow.comments);
+    });
+    
+};
+
+// function to clear the table
+const resetTable = function() {
+    tbody.html('');
+	renderTable(tableData);
+};
+
+// after the user enters an input AND hits Enter (return key), the handler function will execute
+inputDate.on("change", handler);
+inputCity.on("change", handler);
+inputState.on("change", handler);
+inputCountry.on("change", handler);
+inputShape.on("change", handler);
+filterButton.on("click", handler);
+resetButton.on("click", resetTable);
